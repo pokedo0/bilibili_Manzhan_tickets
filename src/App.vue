b9cbd3f3c270b29709fc41b4454064fb13a265fe
LAPTOP-KHEUTB4N\lenovo <2608796981@qq.com>
2023-07-01 10:33:29 +0800

    fix:aaa

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
-    <div>只可以抢默认购票人,只适用miyoSummer</div>
+    <div>只可以抢默认购票人</div>
+    <div>票信息ID（url上有）<input type="text" v-model="piaoId" /></div>
     <div>第 <input type="text" v-model="tian" /> 天</div>
-    <div>钱数<input type="text" v-model="qian" /></div>
+    <div>钱数<input type="text" v-model="qian" />（分）</div>
     <div><input type="text" v-model="time" />秒抢一次</div>
+    <div>
+      抢第
+      <input
+        type="text"
+        v-model="piaoType"
+      />种类型（没有填的话，按钱数判断，若有相同的钱数不能保证抢对票类型）
+    </div>
     <button @click="onlick">开始</button>
   </div>
 </template>
@@ -14,69 +22,77 @@ import axios from "axios";
 const tian = ref();
 const qian = ref();
 const time = ref();
+const piaoId = ref();
+const piaoType = ref();
 const onlick = () => {
+  let grxx;
+  axios({
+    method: "GET",
+    url: `/api/ticket/buyer/list?is_default&projectId=${piaoId.value}`,
+  }).then((res) => {
+    grxx = res.data.data.list;
+    grxx[0].isBuyerInfoVerified = true;
+    grxx[0].isBuyerValid = true;
+  });
   let data = setInterval(
     () => {
       axios({
         method: "GET",
-        url: `/api/ticket/project/get?version=134&id=68575`,
+        url: `/api/ticket/project/get?version=134&id=${piaoId.value}`,
       }).then((res) => {
         let arr = res.data.data;
         let ticketList =
           arr.screen_list[tian.value ? tian.value - 1 : 0].ticket_list; //数组下标代表第几天   //票类型
-        for (let i = 0; i < ticketList.length; i++) {
-          if (ticketList[i].clickable) {
-            Promise.all([
-              //获取个人信息
-              axios({
-                method: "GET",
-                url: `/api/ticket/buyer/list?is_default&projectId=68575`,
-              }),
-              axios({
-                //获取token
-                method: "POST",
-                url: `/api/ticket/order/prepare`,
-                data: {
-                  project_id: 68575,
-                  screen_id:
-                    arr.screen_list[tian.value ? tian.value - 1 : 0].id,
-                  order_type: 1,
-                  count: 1,
-                  sku_id: ticketList[i].id,
-                  token: "",
-                },
-              }),
-            ]).then((res) => {
-              let token = res[1].data.data.token;
-              let grxx = res[0].data.data.list;
-              grxx[0].isBuyerInfoVerified = true;
-              grxx[0].isBuyerValid = true;
-              axios({
-                //抢
-                method: "POST",
-                url: `/api/ticket/order/createV2`,
-                data: {
-                  project_id: 68575,
-                  screen_id:
-                    arr.screen_list[tian.value ? tian.value - 1 : 0].id,
-                  sku_id: ticketList[i].id,
-                  count: 1,
-                  pay_money: +qian.value,
-                  order_type: 1,
-                  timestamp: new Date().getTime(),
-                  token: token,
-                  deviceId: "3c2003ba05634736371935d33a47f77d",
-                  buyer_info: JSON.stringify(grxx),
-                },
-              }).then((res) => {
-                if (res.data.errno===0 && res.data.errtag===0) {
-                  clearInterval(data);
-                  alert("抢到了，请尽快去支付");
-                }
-              });
-            });
-            break;
+        let getPiaoType = 0;
+        if (piaoType.value) {
+          getPiaoType = +piaoType.value - 1;
+        } else {
+          for (let i = 0; i < ticketList.length; i++) {
+            if (ticketList[i].price === +qian.value) {
+              getPiaoType = i;
+              break;
+            }
           }
+        }
+        if (ticketList[getPiaoType].clickable) {
+          axios({
+            //获取token
+            method: "POST",
+            url: `/api/ticket/order/prepare`,
+            data: {
+              project_id: piaoId.value,
+              screen_id: arr.screen_list[tian.value ? tian.value - 1 : 0].id,
+              order_type: 1,
+              count: 1,
+              sku_id: ticketList[getPiaoType].id,
+              token: "",
+            },
+          }).then((res) => {
+            
+            let token = res.data.data.token;
+            axios({
+              //抢
+              method: "POST",
+              url: `/api/ticket/order/createV2`,
+              data: {
+                project_id: piaoId.value,
+                screen_id: arr.screen_list[tian.value ? tian.value - 1 : 0].id,
+                sku_id: ticketList[getPiaoType].id,
+                count: 1,
+                pay_money: +qian.value,
+                order_type: 1,
+                timestamp: new Date().getTime(),
+                token: token,
+                deviceId: "3c2003ba05634736371935d33a47f77d",
+                buyer_info: JSON.stringify(grxx),
+              },
+            }).then((res) => {
+              if (res.data.errno === 0 && res.data.errtag === 0) {
+                clearInterval(data);
+                alert("抢到了，请尽快去支付");
+              }
+            });
+          });
         }
       });
     },
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ let gateway
 let cookie
 gateway = ' https://show.bilibili.com/'
 
-cookie = `你刚刚复制的cookie`
+cookie = `b_nut=1673612630; i-wanna-go-back=-1; _uuid=5883C59A-10598-7837-6173-E1935D9BF55C30415infoc; buvid4=38239556-5668-F439-3473-6AA0AE164DBC32065-023011320-0HQWf7U9Yhl2UOX5oLjtwA%3D%3D; rpdid=|(kmRY|)J|~m0J'uY~JYuk~u); LIVE_BUVID=AUTO3816736995247771; buvid_fp_plain=undefined; nostalgia_conf=-1; is-2022-channel=1; CURRENT_BLACKGAP=0; CURRENT_FNVAL=4048; hit-dyn-v2=1; CURRENT_PID=f81fbd80-cd79-11ed-b7aa-a36dfa7bedc0; b_ut=5; DedeUserID=293942714; DedeUserID__ckMd5=9e2bb0fa9dc16199; buvid3=EF56117D-D130-4425-9686-B6ABAEBD71AF36735infoc; hit-new-style-dyn=0; fingerprint=2575e8faaf2fb4fe93504f52afd4cdda; SESSDATA=c0f3c5ea%2C1703504740%2C8430c%2A61E9MBCnVSqoKEtowiM57GfFzIC36uoUTFdoTPqJGEWgsDRx80JjyMh8hiCHi5Wb_SaCCDgwAADgA; bili_jct=9d3b860e26329d96372526b8a2b21536; sid=8418l3ao; bp_video_offset_293942714=812462646821388300; FEED_LIVE_VERSION=V_NO_BANNER_3; header_theme_version=CLOSE; CURRENT_QUALITY=0; PVID=1; b_lsid=6E742E10D_1890EF91F03; buvid_fp=2575e8faaf2fb4fe93504f52afd4cdda; home_feed_column=4; innersign=0; browser_resolution=641-490; Hm_lvt_909b6959dc6f6524ac44f7d42fc290db=1687858146,1688018347,1688117238,1688177385; msource=pc_web; deviceFingerprint=8d6edf1828fdcf2d60634c4a27369c23; from=pc_search_sug; Hm_lpvt_909b6959dc6f6524ac44f7d42fc290db=1688178599`
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
